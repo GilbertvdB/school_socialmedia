dc185d51ef05b864d9e0c4d2fd8ffc6adae5be91
Gilbert B. <101508384+GilbertvdB@users.noreply.github.com>
2024-07-16 11:07:48 +0200

    Update index.blade.php
    added dropdown menu with parents name in the parent column

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -42,7 +42,32 @@
                                 <td class="py-2 px-4 border-b border-gray-300 text-center">{{ $student->birthdate }}</td>
                                 <td class="py-2 px-4 border-b border-gray-300 text-center">{{ ucfirst($student->gender) }}</td>
                                 <td class="py-2 px-4 border-b border-gray-300 text-center"> {{ $student->classroom_name }}</td>
-                                <td class="py-2 px-4 border-b border-gray-300 text-center">{{ __('Parents') }}</td>
+                                <td class="py-2 px-4 border-b border-gray-300 text-center"> 
+                                    <!-- Settings Dropdown -->
+                                    <div class="hidden sm:flex sm:items-center sm:ms-6">
+                                        <x-dropdown align="left" width="48">
+                                            <x-slot name="trigger">
+                                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                                    <div>{{ __('Parents') }}</div>
+
+                                                    <div class="ms-1">
+                                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                                        </svg>
+                                                    </div>
+                                                </button>
+                                            </x-slot>
+
+                                            <x-slot name="content">
+                                            @foreach( $student->parents as $parent )
+                                                <x-dropdown-link :href="route('profile.edit')">
+                                                    {{ $parent->name }}
+                                                </x-dropdown-link>
+                                            @endforeach
+                                            </x-slot>
+                                        </x-dropdown>
+                                    </div>
+                                </td>
                                 <td class="py-2 px-4 border-b border-gray-300 text-right">
                                     <a href="{{ route('students.edit', $student->id) }}" class="text-blue-600 hover:text-blue-800">Edit</a>
                                     <form action="{{ route('students.destroy', $student->id) }}" method="POST" class="inline">
