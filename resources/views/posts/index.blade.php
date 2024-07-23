b0863e0644031238493e2ab612a2d4f317abee2f
Gilbert B. <101508384+GilbertvdB@users.noreply.github.com>
2024-07-23 23:55:55 +0200

    update dashboard

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-<x-slot name="header">
-    <div class="flex justify-between items-center">
+    <x-slot name="header">
+        <div class="flex justify-between items-center">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Posts') }}
             </h2>
@@ -11,7 +11,7 @@
     </x-slot>
 
     <div class="max-w-6xl mx-auto p-4 sm:p-6 lg:p-8">
-        <div class="mt-4 grid gap-y-4">
+        <div class="mt-4 grid gap-y-8">
             @foreach ($posts as $post)
                 @include('posts.post-box')
             @endforeach
